f353a836e2d82e8992983564e998b7adbf3f6f5b
Huda <hudarashid87@gmail.com>
2023-12-16 16:41:09 +0000

    redo migrations

--- book/models.py
+++ book/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Language(models.Model):
     name = models.CharField(max_length=10)
-    # iso_code = models.CharField(primary_key=True, unique=True, max_length=6)
+    iso_code = models.CharField(max_length=6)
     
     def __str__(self):
         return self.name
@@ -17,7 +17,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     language = models.ForeignKey(Language, on_delete=models.CASCADE, null=True)
-    # language_iso = models.CharField(max_length=6, null=True)
     description = models.TextField()
 
     class Meta:
