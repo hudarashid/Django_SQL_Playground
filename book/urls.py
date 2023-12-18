e1139d5aef35f3f85923a73dfa16e2d7d20fc046
Huda <hudarashid87@gmail.com>
2023-12-18 20:41:07 +0000

    implement url for getting BookDetail

--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from book import views
 
 urlpatterns = [
-    path("book/", views.BookAPIView.as_view()),
+    path("book/<int:pk>/", views.BookDetailAPIView.as_view()),
     path("book-summary/<int:pk>/", views.BookSummaryAPIView.as_view()),
 ]
